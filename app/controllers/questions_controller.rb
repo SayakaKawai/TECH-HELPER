cc6ac9db7a855ef1274d7306740865cbcfeda495
sayaka80 <saya.hinagesi@gmail.com>
2017-02-17 14:36:17 +0900

    質問投稿機能(questions#create)実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,19 @@
 class QuestionsController < ApplicationController
 
   def index
-    @question = Question.all
+    @question = Question.all.order("id DESC")
   end
 
   def new
+  end
+
+  def create
+    Question.create(question_params)
+  end
+
+  private
+  def question_params
+    params.permit(:title, :image, :text)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root   'questions#index'   #ルートパスの指定
-  resources :questions, only: [:index, :new]
+  resources :questions, only: [:index, :new, :create]
 end
