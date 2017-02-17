29ac539cdc0ed1fa99c797629aba6f8dfe386f8a
sayaka80 <saya.hinagesi@gmail.com>
2017-02-17 15:26:42 +0900

    質問の内容を変更

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,12 +8,14 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    Question.create(question_params)
+    new_question = Question.new(question_params)
+    new_question.image = 'http://free-webdesigner.com/fw/wp-content/uploads/2014/10/stationery5.gif'
+    new_question.save
   end
 
   private
   def question_params
-    params.permit(:title, :image, :text)
+    params.permit(:title, :text)
   end
 
 end
